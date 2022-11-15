27ea2ccb1994d7e04641a9393b11ea97922b32ac
paulosanda <paulosanda@gmail.com>
2022-11-15 06:41:43 -0300

    removing methods

--- app/Policies/AgendaPolicy.php
+++ app/Policies/AgendaPolicy.php
@@ -30,17 +30,6 @@ class AgendaPolicy
     }
 
     /**
-     * Determine whether the user can create models.
-     *
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function create(User $user)
-    {
-        //
-    }
-
-    /**
      * Determine whether the user can update the model.
      *
      * @param  \App\Models\User  $user
@@ -51,41 +40,5 @@ class AgendaPolicy
     {
 
         return $user->id === $agenda->user_id;
-    }
-
-    /**
-     * Determine whether the user can delete the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Agenda  $agenda
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function delete(User $user, Agenda $agenda)
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can restore the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Agenda  $agenda
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function restore(User $user, Agenda $agenda)
-    {
-        //
-    }
-
-    /**
-     * Determine whether the user can permanently delete the model.
-     *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\Agenda  $agenda
-     * @return \Illuminate\Auth\Access\Response|bool
-     */
-    public function forceDelete(User $user, Agenda $agenda)
-    {
-        //
     }
 }
